d273d7bad0aff2d87d55ee1812f3846abb6a5558
Vishalkumarjaiswal16 <vishalkumarjaiswal16@gmail.com>
2026-06-29 14:48:59 +0530

    chore: add pyrefly ignore directives to resolve missing import warnings

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, jsonify, request
+# pyrefly: ignore [missing-import]
 from src.helper import download_hugging_face_embeddings
 from langchain_pinecone import PineconeVectorStore
 from langchain_openai import ChatOpenAI
@@ -6,6 +7,7 @@ from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
+# pyrefly: ignore [missing-import]
 from src.prompt import *
 import os
 
